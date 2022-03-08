d2a7c03ab982a396abc9d66f312738df76ec4d46
Oladele Marwan <oladelemarwan@gmail.com>
2022-03-08 17:48:16 +0000

    project page implemented

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <section className="footer">
-        <p>Marwan Oladele &copy;2022</p>
+        <p>Marwan Oladele &copy;{new Date().getFullYear()}</p>
       </section>
     </>
   );
--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import asset1 from "../Assets/asset1.png";
+import asset2 from "../Assets/asset2.png";
 
 const Project = () => {
-  return <div className="project">Project</div>;
+  return (
+    <>
+      <div className="project-main">
+        <h1 className="project-header">My Recent Projects</h1>
+        <div className="project">
+          <img
+            className="projectImage"
+            src={asset2}
+            alt="Product Landing Page"
+          />
+          <h1>Meditech</h1>
+          <p>frontend, 2021</p>
+          <p className="explanation">A Hospital Management Application</p>
+          <div className="stack">
+            <span>React</span>
+            <span>es6</span>
+            <span>Styled Component</span>
+          </div>
+          <a href="https://meditech-react.netlify.app/" target="blank">
+            See Project
+          </a>
+        </div>
+        <div className="project">
+          <img
+            className="projectImage"
+            src={asset1}
+            alt="Product Landing Page"
+          />
+          <h1>Portfolio</h1>
+          <p>frontend, 2022</p>
+          <p className="explanation">A personalized Portfolio design</p>
+          <div className="stack">
+            <span>React</span>
+            <span>es6</span>
+            <span>Styled Component</span>
+          </div>
+          <a href="https://ooladelemarwan.netlify.app" target="blank">
+            See Project
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Project;
