2a410726ad933b9cd8f2b6fa1b7d2076c7194ad7
Oladele Marwan <oladelemarwan@gmail.com>
2022-05-28 19:01:10 +0100

    new commit

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import asset1 from "../Assets/asset1.png";
 import asset2 from "../Assets/asset2.png";
+import asset3 from "../Assets/asset3.png";
 
 const Project = () => {
   return (
     <>
       <div className="project-main">
         <h1 className="project-header">My Recent Projects</h1>
+
         <div className="project">
           <img
             className="projectImage"
-            src={asset2}
-            alt="Product Landing Page"
+            src={asset3}
+            alt="tour app"
           />
-          <h1>Meditech</h1>
-          <p>frontend, 2021</p>
-          <p className="explanation">A Hospital Management Application</p>
+          <h1>Tour App</h1>
+          <p>frontend, 2022</p>
+          <p className="explanation">A tour app used for pick a desired place for</p>
           <div className="stack">
             <span>React</span>
             <span>es6</span>
-            <span>Styled Component</span>
           </div>
-          <a href="https://meditech-react.netlify.app/" target="blank">
+          <a href="https://marwantourproject.netlify.app" target="blank">
             See Project
           </a>
         </div>
@@ -40,6 +41,23 @@ const Project = () => {
             <span>Styled Component</span>
           </div>
           <a href="https://ooladelemarwan.netlify.app" target="blank">
+            See Project
+          </a>
+        </div>
+        <div className="project">
+          <img
+            className="projectImage"
+            src={asset2}
+            alt="Product Landing Page"
+          />
+          <h1>Meditech</h1>
+          <p>frontend, 2021</p>
+          <p className="explanation">A Hospital Management Application</p>
+          <div className="stack">
+            <span>React</span>
+            <span>es6</span>
+          </div>
+          <a href="https://meditech-react.netlify.app/" target="blank">
             See Project
           </a>
         </div>
